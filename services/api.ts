9b4cc2b73782e07a65b34215cc9fa38ffd0b839d
palakg2g <palak1@gulp2go.com>
2025-08-23 12:44:17 +0530

    TMDB setup along with fetchData API and created useFetch custom hook

---
+++ services/api.ts
@@ -0,0 +1,22 @@
+// This object will hold settings and credentials used to make API calls to TMDB.
+export const TMDB_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`, //Bearer authentication is a method of securing API requests.
+  },
+};
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const url = query
+    ? `${TMDB_CONFIG.BASE_URL}/serach/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+  const response = await fetch(url, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+  if (!response.ok) throw new Error("Failed to fetch movies");
+  const data = await response.json();
+  return data.results;
+};
